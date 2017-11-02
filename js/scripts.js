63bf11c6929fa6057b60ea6daba69254d912608b
Holly Wilkalis and Kevin Boyle <holly@>
2017-11-02 14:45:40 -0700

    alternate turns correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 
 var turnValue = [];
+var totalValue = [];
+var bigScore = [];
 //
-function Player(name, roundScore) {
+function Player(name, roundScore, turnValue) {
   this.name = name
   this.calculateScore(); //grabs the score for a single roundScore
   this.totalScore;
-  // this.calculateTotalScore();
+  this.turnValue = turnValue
+  this.calculateTotalScore();
 }
 
 function diceRoll(min, max) {
@@ -22,6 +25,13 @@ Player.prototype.calculateScore = function() {
   }, 0);
   return roundScore;
 } //takes array from singleTurn, converts it to a single value
+
+Player.prototype.calculateTotalScore = function() {
+  var totalScore = turnValue.reduce(function(sum, value) {
+    return sum + value;
+  }, 0);
+  return totalScore;
+} //takes array from all Turns, converts it to a single value
 
 function singleTurn() {
   var pigDice = diceRoll(1, 6);
@@ -59,11 +69,37 @@ $(document).ready(function() {
   });
 
   $("#player1Roll").click(function(event) {
+    event.preventDefault();
+    turnValue = singleTurn();
+    var player1round = player1.calculateScore(turnValue);
+    console.log("player1round " + player1round);
+    console.log("turnValue " + turnValue);
+    var player1total = player1.calculateTotalScore();
+    console.log("player1total " + player1total);
+    if (turnValue.length >=2) {
+      alert("player 1, your time is over");
+      bigScore = totalValue.push(turnValue);
+      var playerYes = player1.calculateTotalScore(bigScore);
+      turnValue.splice(0,2)
+      console.log("pyes " + playerYes);
+    }
+  });//end of player 1 click time
+
+$("#player2Roll").click(function(event) {
   event.preventDefault();
   turnValue = singleTurn();
-  var player1round = player1.calculateScore(turnValue);
-  console.log("player1round " + player1round);
+  var player2round = player2.calculateScore(turnValue);
+  console.log("player2round " + player2round);
   console.log("turnValue " + turnValue);
-  });
+  var player2total = player2.calculateTotalScore();
+  console.log("player2total " + player2total);
+  if (turnValue.length >=2) {
+    alert("player 2, your time is over");
+    bigScore = totalValue.push(turnValue);
+    var playerYes = player2.calculateTotalScore(bigScore);
+    turnValue.splice(0,2);
+    console.log("pyes " + playerYes);
+  }
+  });//end of player 2 click time
 
 });//close document ready function
